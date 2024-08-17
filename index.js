88f3bb9f7c7f79d118c266b81298c5bfe91c681e
Syyst3m <74576840+Xfrisk@users.noreply.github.com>
2024-08-17 00:23:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 const express = require('express');
 const axios = require('axios');
 const rateLimit = require('express-rate-limit');
+const Queue = require('bull');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.set('trust proxy', 1);
+const webhookQueue = new Queue('webhookQueue', 'redis://127.0.0.1:6379');
 
 const webhookRateLimiter = rateLimit({
     windowMs: 60 * 1000,
     max: 25,
     message: { error: 'Too many requests. Please try again later.' },
     statusCode: 429,
+    handler: async (req, res, next, options) => {
+        await webhookQueue.add({
+            webhookId: req.params.webhookId,
+            webhookToken: req.params.webhookToken,
+            body: req.body
+        });
+        res.status(429).send(options.message);
+    }
 });
 
-app.post('/api/webhooks/:webhookId/:webhookToken', webhookRateLimiter, async (req, res) => {
-    const discordWebhookUrl = `https://discord.com/api/webhooks/${req.params.webhookId}/${req.params.webhookToken}`;
+webhookQueue.process(async (job) => {
+    const { webhookId, webhookToken, body } = job.data;
+    const discordWebhookUrl = `https://discord.com/api/webhooks/${webhookId}/${webhookToken}`;
 
     try {
-        const response = await axios.post(discordWebhookUrl, req.body);
-        res.status(200).send('Webhook enviado com sucesso!');
+        await axios.post(discordWebhookUrl, body);
+        console.log('Webhook enviado com sucesso!');
     } catch (error) {
         console.error('Erro ao enviar o webhook:', error.message);
-        res.status(500).send('Erro ao enviar o webhook.');
+        
     }
+});
+
+app.post('/api/webhooks/:webhookId/:webhookToken', webhookRateLimiter, (req, res) => {
+    res.status(200).send('Webhook enfileirado com sucesso!');
 });
 
 app.listen(port, () => {
